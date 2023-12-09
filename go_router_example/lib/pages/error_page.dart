70089f8145c2e7443531a9cd4ef18b435a784790
Farizaaaa <farizaaaa10@gmail.com>
2023-12-09 20:43:37 +0530

    uploading routing in constants

---
+++ go_router_example/lib/pages/error_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ErrorPagePage extends StatelessWidget {
+  const ErrorPagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text("ErrorPagePage")),
+    );
+  }
+}
--- go_router_example/lib/routes/app_route_config.dart
+++ go_router_example/lib/routes/app_route_config.dart
@@ -2,40 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:go_router_example/pages/about_page.dart';
 import 'package:go_router_example/pages/contact_us.dart';
+import 'package:go_router_example/pages/error_page.dart';
 import 'package:go_router_example/pages/home_page.dart';
 import 'package:go_router_example/pages/profie.dart';
+import 'package:go_router_example/routes/app_route_constants.dart';
 
 class MyAppRouter {
   GoRouter router = GoRouter(
-    routes: [
-      GoRoute(
-        path: "/",
-        name: "home",
-        pageBuilder: (context, state) => const MaterialPage(
-          child: HomePage(),
+      routes: [
+        GoRoute(
+          path: "/",
+          name: MyAppRouteConstants.homeRouteName,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: HomePage(),
+          ),
         ),
-      ),
-      GoRoute(
-        path: "/profile",
-        name: "profile",
-        pageBuilder: (context, state) => const MaterialPage(
-          child: ProfilePage(),
+        GoRoute(
+          path: "/profile",
+          name: MyAppRouteConstants.profileRouteName,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: ProfilePage(),
+          ),
         ),
-      ),
-      GoRoute(
-        path: "/about",
-        name: "about",
-        pageBuilder: (context, state) => const MaterialPage(
-          child: AboutPage(),
+        GoRoute(
+          path: "/about",
+          name: MyAppRouteConstants.aboutRouteName,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: AboutPage(),
+          ),
         ),
-      ),
-      GoRoute(
-        path: "/contactus",
-        name: "conatactus",
-        pageBuilder: (context, state) => const MaterialPage(
-          child: ContactUsPage(),
+        GoRoute(
+          path: "/contactus",
+          name: MyAppRouteConstants.contactUsRouteName,
+          pageBuilder: (context, state) => const MaterialPage(
+            child: ContactUsPage(),
+          ),
         ),
-      ),
-    ],
-  );
+      ],
+      errorPageBuilder: (context, state) {
+        return MaterialPage(child: ErrorPagePage());
+      });
 }
